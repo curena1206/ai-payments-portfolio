ef372f04b6aefd8e085db9c874670e73a68a71d1
Carlos Urena <your-email@example.com>
2026-03-13 13:19:40 -0400

    Scroll to top on navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import './index.css'
 
 // Pages
@@ -13,18 +14,26 @@ import Model03 from './models/Model03_CorridorAnalyzer'
 import Model04 from './models/Model04_ClientBehavior'
 import Model05 from './models/Model05_PortfolioScorecard'
 
+// Scroll to top on every route change
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         {/* Landing page */}
         <Route path="/" element={<FrameworkIndex />} />
 
         {/* Individual models */}
-        <Route path="/models/01-profitability"      element={<Model01 />} />
-        <Route path="/models/02-rail-optimizer"     element={<Model02 />} />
-        <Route path="/models/03-corridor-analyzer"  element={<Model03 />} />
-        <Route path="/models/04-client-behavior"    element={<Model04 />} />
+        <Route path="/models/01-profitability"       element={<Model01 />} />
+        <Route path="/models/02-rail-optimizer"      element={<Model02 />} />
+        <Route path="/models/03-corridor-analyzer"   element={<Model03 />} />
+        <Route path="/models/04-client-behavior"     element={<Model04 />} />
         <Route path="/models/05-portfolio-scorecard" element={<Model05 />} />
 
         {/* Catch-all back to index */}
